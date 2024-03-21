98962d681464f84b4acfb21c037220cba3484fe6
jesusaguero <jesus.aguero@tecsup.edu.pe>
2024-03-21 13:05:07 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+
+import { engine } from 'express-handlebars';
+import morgan from 'morgan';
+import { dirname, join } from 'path';
+import { fileURLToPath } from "url";
+
+//inicializar
+const app=express();
+const __dirname=dirname(fileURLToPath(import.meta.url));
+
+//configuraciones
+app.set('port',process.env.PORT || 3000);
+//Carpeta vistas
+app.set('views', join (__dirname,'views'));
+
+app.engine('.hbs',engine({
+    defaultLayout:'main',
+    layoutsDir:join(app.get('views'), 'layouts'),
+    partialsDir:join(app.get('views'), 'partials'),
+    extname:'hbs'
+}));
+app.set('view engine', 'hbs');
+
+//Middlewares
+    app.use(morgan('dev'));
+    app.use(express.urlencoded({extended:false}));
+    app.use(express.json())
+
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.use(express.static(join(__dirname, 'public')))
+
+//ejecutar al servidor
+app.listen(app.get('port'),()=>{
+    console.log("cargando el puerto",app.get('port'))
+})
