d776b0677fe1998235ddd4247ff7ec17a5c640d1
jesusaguero <jesus.aguero@tecsup.edu.pe>
2024-04-04 07:08:26 -0500

    up

--- src/index.js
+++ src/index.js
@@ -13,15 +13,16 @@ const __dirname=dirname(fileURLToPath(import.meta.url));
 //configuraciones
 app.set('port',process.env.PORT || 3000);
 //Carpeta vistas
-app.set('views', join (__dirname,'views'));
 
-app.engine('.hbs',engine({
-    defaultLayout:'main',
-    layoutsDir:join(app.get('views'), 'layouts'),
-    partialsDir:join(app.get('views'), 'partials'),
-    extname:'hbs'
+app.engine('.hbs', engine({
+    extname: '.hbs',
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'views/layouts'),
+    layoutsDir: path.join(__dirname, 'views/partials'),
+
 }));
-app.set('view engine', 'hbs');
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
 
 //Middlewares
     app.use(morgan('dev'));
--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -1,16 +1,23 @@
-import { Router } from 'express';
+import express from 'express';
 import pool from '../database.js';
 
-const router = Router();
+const router = express.Router();
 
-router.get('/list', async(req,res)=>{
-    try{
-        const [result] = await pool.query('SELECT * FROM cliente');
-        res.render('clientes/list',{clientes: result});
-    }
-    catch(err){
-        res.status(500).json({message:err.message});
-    }
+router.get('/add', (req, res) => {
+    res.render('clientes/add');
 });
 
+router.post('/add', async(req, res) => {
+    try{
+        const {nomcli, apecli, nrodnicli, telcli} = req.body;
+        const newCliente = {
+            nomcli, apecli, nrodnicli, telcli
+        }
+        await pool.query('INSERT INTO clientes SET ?', [newCliente]);
+        res.redirect('/list');
+    }
+    catch(err){
+        res.status(500).json({message: err.message});
+    }
+});
 export default router;
